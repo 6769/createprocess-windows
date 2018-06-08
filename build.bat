918b43c88d44d89d36f6b845f5b2fa0696709491
6769 <5pipitk@gmail.com>
2018-06-08 11:08:55 +0800

    vs2017

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
-
-cl /Ox createprocess.cpp -D_USING_V110_SDK71_ /link /FILEALIGN:512 /OPT:REF /OPT:ICF /INCREMENTAL:NO /subsystem:console,5.01 ws2_32.lib /out:createprocess.exe
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" 
+cl /Ox createprocess.cpp -D_USING_V110_SDK71_ /link /FILEALIGN:512 /OPT:REF /OPT:ICF /INCREMENTAL:NO /subsystem:console,6.1 ws2_32.lib /out:createprocess.exe
